e0c7b002cc59b942b11980c2d0558709fac08872
Byron <byron@alchebyte.com>
2019-08-11 13:00:10 -0600

    update Xamarin.Forms package

---
+++ gigIt.Xamarin.UWP/Setup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MvvmCross.Platforms.Uap.Core;
+using MvvmCross.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace gigIt.Xamarin.UWP
+{
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new UwpApp() as IMvxApplication;
+        }
+
+        //protected override IMvxTrace CreateDebugTrace()
+        //{
+        //    return new DebugTrace();
+        //}
+    }
+}
